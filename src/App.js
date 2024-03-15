d0429ea8e369456e75579e727f509ac9ee5faaaf
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-03-14 22:41:25 -0400

    active navlink working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Nav from "./components/Nav.jsx";
 import { Routes, Route } from "react-router-dom";
 import Home from "./screens/Home.jsx";
+import About from "./screens/About.jsx";
 import './App.css';
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={ <Home /> }/>
+        <Route path="/about" element={ <About /> }/>
+
       </Routes>
     </div>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,24 +1,63 @@
 import {NavLink} from 'react-router-dom';
+import { useState } from 'react';
 import "./Nav.css";
 
 
 function Nav() {
+  
+  
   return (
     <div id="background-Nav">
+      <NavLink 
+        id="home-Nav" 
+        to="/" 
+        className={({ isActive }) => (isActive ? 'active-Nav' : 'link-Nav')}
+      >
+        <img 
+          src="../../images/Logo_GOOEY_H_Soft2.png" 
+          alt="hyp access logo" 
+          id="navLogo-Nav"
+        />
+        <p className='link-Nav'>hyp + access</p>
+      </NavLink>
       <div id="navContainer-Nav">
-          <NavLink id="home-Nav" className="link-Nav" to="/">
-            <img 
-              src="../../images/Logo_GOOEY_H_Soft2.png" 
-              alt="hyp access logo" 
-              id="navLogo-Nav"
-            />
+
+          <NavLink 
+             to="/about" 
+            className={({ isActive }) => (isActive ? 'active-Nav' : 'link-Nav')}
+          >
+              about
           </NavLink>
 
-          <NavLink className="link-Nav" to="/about">about</NavLink>
-          <NavLink className="link-Nav" to="/resources">resources</NavLink>
-          <NavLink className="link-Nav" to="/research">research</NavLink>
-          <NavLink className="link-Nav" to="/care">care</NavLink>
-          <NavLink className="link-Nav" to="/giving">giving</NavLink>
+          <NavLink 
+             to="/resources" 
+            className={({ isActive }) => (isActive ? 'active-Nav' : 'link-Nav')}
+          >
+              resources
+          </NavLink>
+
+          <NavLink 
+             to="/resarch" 
+            className={({ isActive }) => (isActive ? 'active-Nav' : 'link-Nav')}
+          >
+              resarch
+          </NavLink>
+
+          <NavLink 
+             to="/care" 
+            className={({ isActive }) => (isActive ? 'active-Nav' : 'link-Nav')}
+          >
+              care
+          </NavLink>
+
+          <NavLink 
+             to="/giving" 
+            className={({ isActive }) => (isActive ? 'active-Nav' : 'link-Nav')}
+          >
+              giving
+          </NavLink>
+
+          
           
       </div>
     </div>
